19ebff5213a9bb612378a5a41e170344b181b03f
NeKosmic-NK <104524366+NeKosmic-NK@users.noreply.github.com>
2022-08-04 19:05:51 +0100

    Create spotify.js

---
+++ plugins/spotify.js
@@ -0,0 +1,72 @@
+const { default: makeWASocket, BufferJSON, WA_DEFAULT_EPHEMERAL, generateWAMessageFromContent, downloadContentFromMessage, downloadHistory, proto, getMessage, generateWAMessageContent, prepareWAMessageMedia } = require('@adiwajshing/baileys')
+let fs = require('fs')
+let path = require('path')
+let { MessageType } = require('@adiwajshing/baileys')
+let PhoneNumber = require('awesome-phonenumber')
+let levelling = require('../lib/levelling')
+let handler = async (m, { conn, usedPrefix }) => {
+let user = global.db.data.users[m.sender]
+let pp = './Menu2.jpg'
+let who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
+try {
+} catch (e) {
+} finally {
+let { name, limit, exp, banned, lastclaim, registered, regTime, age, level } = global.db.data.users[m.sender]
+let { min, xp, max } = levelling.xpRange(level, global.multiplier)
+let username = conn.getName(who)
+let menu = `
+
+Spotify
+12terren12@gmail.com
+11terren11@gmail.com 
+gegak52312@yastle.com
+miriwif460@sunyds.com
+costaricapurav11@gmailcom
+mincho8979
+
+`.trim()
+let mentionedJid = [who]
+const template = generateWAMessageFromContent(m.chat, proto.Message.fromObject({
+templateMessage: {
+hydratedTemplate: {
+hydratedContentText: menu,
+locationMessage: { 
+jpegThumbnail: fs.readFileSync('./Menu2.jpg') },
+hydratedFooterText: '©𝑆𝑖𝑚𝑝𝑙𝑒𝐵𝑜𝑡',
+hydratedButtons: [{
+urlButton: {
+displayText: '𝙸𝙽𝚂𝚃𝙰𝙶𝚁𝙰𝙼',
+url: 'https://www.instagram.com/invites/contact/?i=5yv9hdjlc3sw&utm_content=ohvhscn'
+}},
+{
+urlButton: {
+displayText: '︎𝙾𝚆𝙽𝙴𝚁',
+url: 'https://wa.me/34623442554'    
+}},
+{
+quickReplyButton: {
+displayText: null,
+id: null,
+}},
+{
+quickReplyButton: {
+displayText: null,
+id: null,
+}},
+{
+quickReplyButton: {
+displayText: null,
+id: null,
+}}]}}
+}), { userJid: m.sender, quoted: m });
+return await conn.relayMessage(
+m.chat,
+template.message,
+{ messageId: template.key.id })    
+    
+}}
+handler.help = ['menu', 'help', '?']
+handler.tags = ['general']
+handler.command = /^(menucompleto|comandos|allmenu|info|speed|estado|menú|menu|help|\?)$/i
+handler.fail = null
+module.exports = handler
