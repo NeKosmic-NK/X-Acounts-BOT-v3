a2a8a466e76772a0b8ffdf5e827ebb26e30e8cdb
NeKosmic-NK <104524366+NeKosmic-NK@users.noreply.github.com>
2022-08-05 14:17:27 +0100

    Update menu.js

--- plugins/menu.js
+++ plugins/menu.js
@@ -59,38 +59,8 @@ hydratedTemplate: {
 hydratedContentText: menu,
 locationMessage: { 
 jpegThumbnail: fs.readFileSync('./Menu2.jpg') },
-hydratedFooterText: '©𝑆𝑖𝑚𝑝𝑙𝑒𝐵𝑜𝑡',
-hydratedButtons: [{
-urlButton: {
-displayText: '𝙸𝙽𝚂𝚃𝙰𝙶𝚁𝙰𝙼',
-url: 'https://www.instagram.com/invites/contact/?i=5yv9hdjlc3sw&utm_content=ohvhscn'
-}},
-{
-urlButton: {
-displayText: '︎𝙾𝚆𝙽𝙴𝚁',
-url: 'https://wa.me/34623442554'    
-}},
-{
-quickReplyButton: {
-displayText: null,
-id: null,
-}},
-{
-quickReplyButton: {
-displayText: null,
-id: null,
-}},
-{
-quickReplyButton: {
-displayText: null,
-id: null,
-}}]}}
-}), { userJid: m.sender, quoted: m });
-return await conn.relayMessage(
-m.chat,
-template.message,
-{ messageId: template.key.id })    
-    
+hydratedFooterText: '©𝑆𝑖𝑚𝑝𝑙𝑒𝐵𝑜𝑡' })  
+
 }}
 handler.help = ['menu', 'help', '?']
 handler.tags = ['general']
