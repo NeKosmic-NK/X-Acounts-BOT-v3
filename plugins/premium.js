0cffd891a4ccf49f21a8c33cd892eb6ca7be1b96
NeKosmic-NK <104524366+NeKosmic-NK@users.noreply.github.com>
2022-08-09 15:47:06 +0100

    Create premium.js

---
+++ plugins/premium.js
@@ -0,0 +1,45 @@
+const { default: makeWASocket, BufferJSON, WA_DEFAULT_EPHEMERAL, generateWAMessageFromContent, downloadContentFromMessage, downloadHistory, proto, getMessage, generateWAMessageContent, prepareWAMessageMedia } = require('@adiwajshing/baileys')
+let fs = require('fs')
+let path = require('path')
+let { MessageType } = require('@adiwajshing/baileys')
+let PhoneNumber = require('awesome-phonenumber')
+let levelling = require('../lib/levelling')
+let handler = async (m, { conn, usedPrefix }) => {
+let user = global.db.data.users[m.sender]
+let pp = './Menu2.jpg'
+let who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
+try {
+} catch (e) {
+} finally {
+let { name, limit, exp, banned, lastclaim, registered, regTime, age, level } = global.db.data.users[m.sender]
+let { min, xp, max } = levelling.xpRange(level, global.multiplier)
+let username = conn.getName(who)
+let menu = `
+
+Para usar este comando debes ser un usuario Premium, o administrador del Bot.
+-----------------------------------------------------------------------------
+Que ofrece este comando?
+------------------------
+Tendras acceso a cuentas premium inusuales, como por ejemplo YT premium etc...
+------------------------------------------------------------------------------
+BOT NUEVO
+COMANDO NUEVO.
+POCAS CUENTAS DISPONIBLES POR EL MOMENTO.
+-----------------------------------------
+
+`.trim()
+let mentionedJid = [who]
+/*const template = generateWAMessageFromContent(m.chat, proto.Message.fromObject({
+templateMessage: {
+hydratedTemplate: {
+hydratedContentText: menu,
+locationMessage: { 
+jpegThumbnail: fs.readFileSync('./Menu2.jpg') },
+hydratedFooterText: '©𝑆𝑖𝑚𝑝𝑙𝑒𝐵𝑜𝑡' })*/  
+conn.sendFile(m.chat, pp, 'error.jpg', menu, m )
+}}
+handler.help = ['menu', 'help', '?']
+handler.tags = ['general']
+handler.command = /^(premium|\?)$/i
+handler.fail = null
+module.exports = handler
