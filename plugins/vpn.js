0ee1494ff206c45aef0efcf5e94bc738aa3d643c
NeKosmic-NK <104524366+NeKosmic-NK@users.noreply.github.com>
2022-08-18 16:10:52 +0100

    Update vpn.js

--- plugins/vpn.js
+++ plugins/vpn.js
@@ -16,7 +16,30 @@ let { min, xp, max } = levelling.xpRange(level, global.multiplier)
 let username = conn.getName(who)
 let menu = `
 
-NO DISPONIBLE POR EL MOMENTO.
+Hits NORD
+
+simpsonjff@gmail.com:prosno77
+jake.mcpherson88@gmail.com:Heaven420!
+jfplopes@gmail.com:Joalopes2
+depenne@hotmail.nl:Tijn1971
+pdonk009@gmail.com:pdonk009
+mylesmalone14@gmail.com:Skyline14
+ifiery12@gmail.com:exdragon565
+kr3wsk8er45@gmail.com:cool9000
+ashycole65@yahoo.com:Ashley090903
+mickelinghall@gmail.com:mikaelli1993
+morrisben96@gmail.com:jammin34
+jacksonbuckingham1@gmail.com:Buckingham1
+langkynoose@gmail.com:asdfasdf
+akashiya221@gmail.com:Vampire20
+aleonenko9988@gmail.com:Nexusfire9
+bobstemm@gmail.com:n3Ighb0r
+frankglaspey@gmail.com:fg54197700
+oheneyd@gmail.com:Peanuts123
+tcsufan03@yahoo.com:tylerc62
+fazewolflink1337@gmail.com:Isaiah724
+
+@Mephisto178
 
 `.trim()
 let mentionedJid = [who]
